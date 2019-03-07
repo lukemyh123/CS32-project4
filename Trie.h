7d10e831c31d6dbf18ef619c7a73105f7a984c34
lukemyh123 <46793748+lukemyh123@users.noreply.github.com>
2019-03-06 21:30:41 -0800

    Add files via upload

---
+++ Trie.h
@@ -0,0 +1,45 @@
+#ifndef TRIE_INCLUDED
+#define TRIE_INCLUDED
+
+#include <string>
+#include <vector>
+
+template<typename ValueType>
+class Trie
+{
+public:
+    Trie();
+    ~Trie();
+    void reset();
+    void insert(const std::string& key, const ValueType& value);
+    std::vector<ValueType> find(const std::string& key, bool exactMatchOnly) const;
+    
+    // C++11 syntax for preventing copying and assignment
+    Trie(const Trie&) = delete;
+    Trie& operator=(const Trie&) = delete;
+private:
+    struct tNode
+    {
+        char c;
+        vector<ValueType> values;
+        tNode* children[5];
+    }
+    tNode *root;
+};
+
+Trie::Trie()
+{
+    root->values = nullptr;
+}
+
+void Trie::insert(const std::string& key, const ValueType& value)
+{
+    
+}
+
+std::vector<ValueType> Trie::find(const std::string& key, bool exactMatchOnly) const
+{
+    
+}
+#endif // TRIE_INCLUDED
+
