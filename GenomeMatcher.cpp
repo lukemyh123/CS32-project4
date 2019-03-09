931cb32e0b249cc56e333c4efb206dacfb876e3d
lukemyh123 <46793748+lukemyh123@users.noreply.github.com>
2019-03-09 13:37:06 -0800

    Add files via upload

--- GenomeMatcher.cpp
+++ GenomeMatcher.cpp
@@ -1,4 +1,5 @@
 #include "provided.h"
+#include "Trie.h"
 #include <string>
 #include <vector>
 #include <iostream>
@@ -8,37 +9,100 @@ using namespace std;
 class GenomeMatcherImpl
 {
 public:
-    GenomeMatcherImpl(int minSearchLength);
-    void addGenome(const Genome& genome);
-    int minimumSearchLength() const;
-    bool findGenomesWithThisDNA(const string& fragment, int minimumLength, bool exactMatchOnly, vector<DNAMatch>& matches) const;
-    bool findRelatedGenomes(const Genome& query, int fragmentMatchLength, bool exactMatchOnly, double matchPercentThreshold, vector<GenomeMatch>& results) const;
+	GenomeMatcherImpl(int minSearchLength);
+	void addGenome(const Genome& genome);
+	int minimumSearchLength() const;
+	bool findGenomesWithThisDNA(const string& fragment, int minimumLength, bool exactMatchOnly, vector<DNAMatch>& matches) const;
+	bool findRelatedGenomes(const Genome& query, int fragmentMatchLength, bool exactMatchOnly, double matchPercentThreshold, vector<GenomeMatch>& results) const;
+	
+	void test();
 private:
+	int m_minSearchLength;  //for findGenomesWithThisDNA()
+
+	struct nameAndPos
+	{
+		string genome_name;
+		int pos;
+	};
+
+	Trie<nameAndPos*> trie;
+
 };
+
+void GenomeMatcherImpl::test()
+{
+	vector<nameAndPos*> temp = trie.find("GAAG", true);
+	for (vector<nameAndPos*>::iterator it = temp.begin(); it != temp.end(); ++it)
+		cout << (*it)->genome_name << endl;
+}
 
 GenomeMatcherImpl::GenomeMatcherImpl(int minSearchLength)
 {
-    // This compiles, but may not be correct
+	m_minSearchLength = minSearchLength;
 }
 
 void GenomeMatcherImpl::addGenome(const Genome& genome)
 {
-    // This compiles, but may not be correct
+	if (minimumSearchLength() >= genome.length())
+	{
+		string dna_sequency;
+		if (genome.extract(0, minimumSearchLength(), dna_sequency))
+		{
+			nameAndPos* temp = new nameAndPos;
+			temp->genome_name = genome.name();
+			temp->pos = 0;
+			trie.insert(dna_sequency, temp);
+		}
+	}
+	else
+	{
+		for (int i = 0; i <= (genome.length() - minimumSearchLength()); i++)
+		{
+			string dna_sequency;
+			if (genome.extract(i, minimumSearchLength(), dna_sequency))
+			{
+				nameAndPos* temp = new nameAndPos;
+				temp->genome_name = genome.name();
+				temp->pos = i;
+				trie.insert(dna_sequency, temp);
+			}
+		}
+	}
 }
 
 int GenomeMatcherImpl::minimumSearchLength() const
 {
-    return 0;  // This compiles, but may not be correct
+	return m_minSearchLength;
 }
 
 bool GenomeMatcherImpl::findGenomesWithThisDNA(const string& fragment, int minimumLength, bool exactMatchOnly, vector<DNAMatch>& matches) const
 {
-    return false;  // This compiles, but may not be correct
+	if(fragment.size() < minimumLength)
+		return false;
+	if (minimumLength < minimumSearchLength())
+		return false;
+
+	string prefi_sequence;
+	for (int i = 0; i < minimumSearchLength(); i++)
+	{
+		prefi_sequence += fragment[i];
+	}
+
+	/*vector<int> temp = trie.find(prefi_sequence, true);
+	for (vector<int>::iterator it = temp.begin(); it != temp.end(); ++it)
+		cout << (*it) << endl;*/
+
+	//DNAMatch *new_match = new DNAMatch;
+	//new_match->genomeName = name;
+	//new_match->position = position;
+	//new_match->length = length;
+	//matches.push_back(new_match);
+	
 }
 
 bool GenomeMatcherImpl::findRelatedGenomes(const Genome& query, int fragmentMatchLength, bool exactMatchOnly, double matchPercentThreshold, vector<GenomeMatch>& results) const
 {
-    return false;  // This compiles, but may not be correct
+	return false;  // This compiles, but may not be correct
 }
 
 //******************** GenomeMatcher functions ********************************
@@ -48,30 +112,35 @@ bool GenomeMatcherImpl::findRelatedGenomes(const Genome& query, int fragmentMatc
 
 GenomeMatcher::GenomeMatcher(int minSearchLength)
 {
-    m_impl = new GenomeMatcherImpl(minSearchLength);
+	m_impl = new GenomeMatcherImpl(minSearchLength);
 }
 
 GenomeMatcher::~GenomeMatcher()
 {
-    delete m_impl;
+	delete m_impl;
 }
 
 void GenomeMatcher::addGenome(const Genome& genome)
 {
-    m_impl->addGenome(genome);
+	m_impl->addGenome(genome);
 }
 
 int GenomeMatcher::minimumSearchLength() const
 {
-    return m_impl->minimumSearchLength();
+	return m_impl->minimumSearchLength();
+}
+
+void GenomeMatcher::test()
+{
+	m_impl->test();
 }
 
 bool GenomeMatcher::findGenomesWithThisDNA(const string& fragment, int minimumLength, bool exactMatchOnly, vector<DNAMatch>& matches) const
 {
-    return m_impl->findGenomesWithThisDNA(fragment, minimumLength, exactMatchOnly, matches);
+	return m_impl->findGenomesWithThisDNA(fragment, minimumLength, exactMatchOnly, matches);
 }
 
 bool GenomeMatcher::findRelatedGenomes(const Genome& query, int fragmentMatchLength, bool exactMatchOnly, double matchPercentThreshold, vector<GenomeMatch>& results) const
 {
-    return m_impl->findRelatedGenomes(query, fragmentMatchLength, exactMatchOnly, matchPercentThreshold, results);
+	return m_impl->findRelatedGenomes(query, fragmentMatchLength, exactMatchOnly, matchPercentThreshold, results);
 }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 //  project4
 //
 //  Created by yunhui ma on 3/6/19.
-//  Copyright Â© 2019 luke. All rights reserved.
+//  Copyright © 2019 luke. All rights reserved.
 //
 #include <fstream>     //    needed    to    open    files
 #include <string>
@@ -14,73 +14,76 @@
 #include "provided.h"
 using namespace std;
 int main() {
-    
-   /* Trie<int>    trie;        //    This    is    like    std::multimap<std::string,int>    trie;
-    trie.insert("GATTACA",    42);        //    GATTACA    Ã  {42}
-     trie.insert("GATTACA",    17);       //    GATTACA    Ã  {42,    17}
-     trie.insert("GATTACA",    42);
-    trie.insert("GATTAjA",    989);
-    trie.insert("GgTTACA",    9989);
 
-    //trie.insert("CA",    10);        //    GATTACA    Ã  {42}
-    //trie.insert("GA",    42);        //    GATTACA    Ã  {42}
-    trie.insert("GKb", 17);       //    GATTACA    Ã  {42,    17}
-    trie.insert("GKb", 42);
-    trie.insert("GKb", 5);
-    trie.insert("GKjjjjjjjjjjjiiohiguigfufuyfuyfb", 56);
-    trie.insert("GKjjjjjjjjjjjiiohiguigfuguyfuyfb", 999);
+	/* Trie<int>    trie;        //    This    is    like    std::multimap<std::string,int>    trie;
+	 trie.insert("GATTACA",    42);        //    GATTACA    à {42}
+	  trie.insert("GATTACA",    17);       //    GATTACA    à {42,    17}
+	  trie.insert("GATTACA",    42);
+	 trie.insert("GATTAjA",    989);
+	 trie.insert("GgTTACA",    9989);
+	 //trie.insert("CA",    10);        //    GATTACA    à {42}
+	 //trie.insert("GA",    42);        //    GATTACA    à {42}
+	 trie.insert("GKb", 17);       //    GATTACA    à {42,    17}
+	 trie.insert("GKb", 42);
+	 trie.insert("GKb", 5);
+	 trie.insert("GKjjjjjjjjjjjiiohiguigfufuyfuyfb", 56);
+	 trie.insert("GKjjjjjjjjjjjiiohiguigfuguyfuyfb", 999);
+	 trie.insert("jKa", 999);
+	 trie.insert("GKa", 100);
+	 trie.insert("GKa", 196);
+	 trie.insert("Gob", 888);
+	 trie.insert("KKT", 42);
 
-    trie.insert("jKa", 999);
+	 //trie.print();
+	 vector<int> temp = trie.find("GATTACA", false);//    GATTACA    à {42,    17,    42}
+	 for (vector<int>::iterator it = temp.begin();
+		  it != temp.end(); ++it)
+		 cout << (*it) << " ";
+	 cout<<endl;
 
-    trie.insert("GKa", 100);
-    trie.insert("GKa", 196);
-    trie.insert("Gob", 888);
+	 trie.reset();
 
-    trie.insert("KKT", 42);
-    
-    //trie.print();
-    vector<int> temp = trie.find("GATTACA", false);//    GATTACA    Ã  {42,    17,    42}
-    for (vector<int>::iterator it = temp.begin();
-         it != temp.end(); ++it)
-        cout << (*it) << " ";
-    cout<<endl;
-    
-    trie.reset();
-    
-    trie.insert("Gob", 888);
+	 trie.insert("Gob", 888);
+	 vector<int> tempr = trie.find("GATTACA", false);//    GATTACA    à {42,    17,    42}
+	 for (vector<int>::iterator it = tempr.begin();
+		  it != tempr.end(); ++it)
+		 cout << (*it) << " ";*/
+		 /* string filename = "/Users/yunhuima/Documents/CS32/Gee-nomics/data/test.txt";
+		  ifstream strm(filename);
+		  if(!strm)
+		  {
+			  cout << "Cannot open " << filename << endl;
+			  return 0;
+		  }
+		  vector<Genome> vg;
+		  bool success = Genome::load(strm, vg);
+		  if(success)
+		  {
+			  cout << "Loaded " <<vg.size()<<" genomes successfully:" <<endl;
+			  for(int k = 0; k != vg.size(); k++)
+				  cout << vg[k].length() << " " << vg[k].name() << endl;
+		  }
+		  else
+			  cout << "Error loading genome data"<<endl;
+		  */
 
-    vector<int> tempr = trie.find("GATTACA", false);//    GATTACA    Ã  {42,    17,    42}
-    for (vector<int>::iterator it = tempr.begin();
-         it != tempr.end(); ++it)
-        cout << (*it) << " ";*/
-   /* string filename = "/Users/yunhuima/Documents/CS32/Gee-nomics/data/test.txt";
+	Genome g1("Genome1","CGGTGTACNACGACTGGGGATAGAATATCTTGACGTCGTACCGGTTGTAGTCGTTCGACCGAAGGGTTCCGCGCCAGTAC");
+	Genome g2("Genome2","TAACAGAGCGGTNATATTGTTACGAATCACGTGCGAGACTTAGAGCCAGAATATGAAGTAGTGATTCAGCAACCAAGCGG");
+	Genome g3("Genome3","TTTTGAGCCAGCGACGCGGCTTGCTTAACGAAGCGGAAGAGTAGGTTGGACACATTNGGCGGCACAGCGCTTTTGAGCCA");
 
-    ifstream strm(filename);
-    if(!strm)
-    {
-        cout << "Cannot open " << filename << endl;
-        return 0;
-    }
-    vector<Genome> vg;
-    bool success = Genome::load(strm, vg);
-    if(success)
-    {
-        cout << "Loaded " <<vg.size()<<" genomes successfully:" <<endl;
-        for(int k = 0; k != vg.size(); k++)
-            cout << vg[k].length() << " " << vg[k].name() << endl;
-    }
-    else
-        cout << "Error loading genome data"<<endl;
-    */
-    
-    Genome g("oryx",
-             "GCTCGGNACACATCCGCCGCGGACGGGACGGGATTCGGGCTGTCGATTGTCTCACAGATCGTCGACGTACATGACTGGGA");
-    
-    string f1, f2, f3;
-    bool result1 = g.extract(0, 5, f1);     //    result1    =    true,    f1    =    â€œGCTCGâ€;
-    bool result2 = g.extract(74, 6, f2); //    result2 =    true,    f2 =    â€œCTGGGAâ€;
-    bool result3 = g.extract(74, 7, f3); //    result3 =    false,    f3 is    unchanged
-    
-    cout << result3 <<endl;
-    return 0;
+	GenomeMatcher gm(4);
+
+	gm.addGenome(g1);
+	gm.addGenome(g2);
+	gm.addGenome(g3);
+
+	gm.test();
+
+	/*string f1, f2, f3;
+	bool result1 = g.extract(0, 5, f1);     //    result1    =    true,    f1    =    “GCTCG”;
+	bool result2 = g.extract(74, 6, f2); //    result2 =    true,    f2 =    “CTGGGA”;
+	bool result3 = g.extract(74, 7, f3); //    result3 =    false,    f3 is    unchanged*/
+
+	//cout << f1 << endl;
+	return 0;
 }
